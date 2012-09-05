58d547526a3ac2469b0bb8e0213a81afed50ebc7
Anant Narayanan <anant@kix.in>
2012-09-05 03:21:04 -0700

    Add ejs for templating

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require("express"),
     app     = express();
 
-app.get("/", function(req, res) {
-  res.redirect("/index.html");
-});
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.bodyParser());
+app.use(express.cookieParser("thisistehsecret"));
+
+app.use(express.session());
 app.use(express.static(__dirname + "/static"));
+
+app.get("/", function(req, res) {
+  res.render("login");
+});
 
 app.listen(3000);
